d0b8f2b5121313e8d685b1a440e5b00202e0b070
Manju <manjshettar@gmail.com>
2024-11-04 19:26:56 -0800

    p

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
     "fmt"
     "net/http"
     "strconv"
+    "encoding/json"
+    "io"
 )
 
 type post struct {
@@ -15,63 +17,92 @@ type user struct {
     userID string
     name string
     email string
+    password string
     posts []post
 }
 
-func createNewUser(userID string, name string, email string) *user{
-    u := user{userID:userID, name:name, email:email}
+type registration struct {
+    Username string `json:"username"`
+    Password string `json:"password"`
+    Email string `json:"email"`
+}
+
+var userId int = 0
+var user_map map[string]user = make(map[string]user)
+
+func parseRegistrationRequest(body io.ReadCloser) (*registration, error){
+    decoder := json.NewDecoder(body)
+    var reg registration
+    err := decoder.Decode(&reg)
+
+    defer body.Close()
+
+    return &reg, err
+}
+
+func incrementUserIDOnNewUser() string {
+    userId += 1
+    return strconv.Itoa(userId)
+}
+
+func createNewUser(userID string, name string, email string, password string) *user{
+    u := user{userID:userID, name:name, email:email, password: password}
     u.posts = make([]post, 0, 10)
     return &u
 }
 
-func main(){
-    userId := 0
-    user_map := make(map[string]user)
+func register(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "POST"{
+        reg, err := parseRegistrationRequest(r.Body)
+        
+        if err != nil{
+            http.Error(w, "Invalid request body", http.StatusBadRequest)
+            return
+        }
+        
+        name := reg.Username
+        email := reg.Email
+        password := reg.Password
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hello World!")
-    })
-    
-    http.HandleFunc("/createUser", func(w http.ResponseWriter, r *http.Request){
-        if r.Method == "GET"{
-            name := r.URL.Query().Get("name")
-            email := r.URL.Query().Get("email")
+        strRep := incrementUserIDOnNewUser()
+
+        newUsr := createNewUser(strRep, name, email, password)
+        user_map[strRep] = *newUsr 
+        fmt.Fprintf(w, "User %v created.\n", name)
+    } else{
+        fmt.Fprintf(w, "Invalid request.\n")
+    }
+}
 
 
-            curId := userId + 1
-            strRep := strconv.Itoa(curId)
-            
-            userId += 1
-          
-            newUsr := createNewUser(strRep, name, email)
-            user_map[strRep] = *newUsr 
-            fmt.Fprintf(w, "User %v created.\n", name)
+func getAll(w http.ResponseWriter, r *http.Request) {
+    str := ""
+    for _, usr := range user_map{
+        str += "Name: " + usr.name + " email: " + usr.email + " id: " + usr.userID + "\n"
+    }
+    fmt.Fprintf(w, str)
+}
+
+func getByID(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "GET"{
+        id := r.URL.Query().Get("id")
+
+        val, ok := user_map[id]
+        if !ok{
+            fmt.Fprintf(w, "No user by that name\n")
         } else{
-            fmt.Fprintf(w, "Invalid request.\n")
+            fmt.Fprintf(w, "User: %v, email: %v, id: %v\n", val.name, val.email, val.userID)
         }
-    }) 
+    }
+}
+
+func main(){
+
+    http.HandleFunc("/register", register) 
     
+    http.HandleFunc("/getAll", getAll)
 
-    http.HandleFunc("/getAll", func(w http.ResponseWriter, r *http.Request){
-        str := ""
-        for id, usr := range user_map{
-            str += "Name: " + usr.name + " email: " + usr.email + " id: " + usr.id + "\n"
-        }
-        fmt.Fprintf(w, str)
-    })
-
-    http.HandleFunc("/getUser", func(w http.ResponseWriter, r *http.Request){
-        if r.Method == "GET"{
-            id := r.URL.Query().Get("id")
-
-            val, ok := user_map[id]
-            if !ok{
-                fmt.Fprintf(w, "No user by that name\n")
-            } else{
-                fmt.Fprintf(w, "User: %v, email: %v, id: %v\n", val.name, val.email, val.userID)
-            }
-        }
-    })
+    http.HandleFunc("/getUser", getByID)
 
     fmt.Println("Server starting on port 8000")
     http.ListenAndServe(":8000", nil)
